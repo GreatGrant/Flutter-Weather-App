560c99e9e3dcb9e49883241e280c4124ddb0195b
Great Grant <greatgrant116@gmail.com>
2024-01-02 23:50:12 +0100

    refactor to extract widgets

--- lib/screens/pages/waether_screen.dart
+++ lib/screens/pages/waether_screen.dart
@@ -73,40 +73,17 @@ class HomePageState extends State<HomePage>{
             ),
             ),
             const SizedBox(height:  16),
-            Row(
-              children: [
-                Card(
-                  elevation: 6,
-                    child: Container(
-                      width: 100,
-                      padding: const EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: const Column(
-                        children: [
-                          Text(
-                            "09:00",
-                            style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold
-                            ),
-                          ),
-                          SizedBox(height: 8),
-                          Icon(
-                            Icons.cloud,
-                            size: 32,
-                          ),
-                          SizedBox(height: 8),
-                          Text(
-                            "3:20.12",
-                          ),
-                        ],
-                      ),
-                    ),
-
-                  ),
-              ],
+            const SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  HourlyForecastCardItem(time: "09:00", temperature: "7.67", icon: Icons.cloud),
+                  HourlyForecastCardItem(time: "10:00", temperature: "7.69", icon: Icons.cloud),
+                  HourlyForecastCardItem(time: "11:00", temperature: "6.67", icon: Icons.cloud),
+                  HourlyForecastCardItem(time: "12:00", temperature: "5.67", icon: Icons.cloud),
+                  HourlyForecastCardItem(time: "01:00", temperature: "7.70", icon: Icons.cloud),
+                ],
+              ),
             ),
             Placeholder(fallbackHeight: 150),
             SizedBox(height: 20),
@@ -117,4 +94,52 @@ class HomePageState extends State<HomePage>{
     );
   }
 
+}
+
+class HourlyForecastCardItem extends StatelessWidget {
+  const HourlyForecastCardItem({
+    super.key,
+    required this.time,
+    required this.temperature,
+    required this.icon,
+  });
+
+  final String time;
+  final String temperature;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+        child: Container(
+          width: 100,
+          padding: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child:  Column(
+            children: [
+              Text(
+                time,
+                style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              const SizedBox(height: 8),
+               Icon(
+                icon,
+                size: 32,
+              ),
+              const SizedBox(height: 8),
+              Text(
+                temperature,
+              ),
+            ],
+          ),
+        ),
+
+      );
+  }
 }
